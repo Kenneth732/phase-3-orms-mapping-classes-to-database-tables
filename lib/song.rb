57e0d0a1b89cdb792cea899dba505bbb4a4ac9f7
KENNETH <kenneth.mburu@student.moringaschool.com>
2023-02-20 12:44:40 +0300

    Update song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,46 +1,10 @@
 class Song
 
-  attr_accessor :name, :album, :id
+  attr_accessor :name, :album
 
-  def initialize(name:, album:, id: nil)
-    @id = id
+  def initialize(name:, album:)
     @name = name
     @album = album
   end
 
-  def self.create_table
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        album TEXT
-        )
-        SQL
-    DB[:conn].execute(sql)
-  end
-
-  def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.album)
-
-  end
-
-  def self.create(name:, album:)
-    song = Song.new(name: name, album:: album)
-    song.save
-  end
 end
-
-
-song = Song.create(name: "Hello", album: "25")
-# => #<Song:0x007f94f2c28ee8 @id=1, @name="Hello", @album="25">
-
-song.name
-# => "Hello"
-
-song.album
-# => "25"
